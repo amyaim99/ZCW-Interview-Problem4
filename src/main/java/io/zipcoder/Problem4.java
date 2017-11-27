9e6f941d8252866d21d7295ad4073fee680eb484
amyaim <amyaim99@gmail.com>
2017-11-27 17:43:13 -0500

    done

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,4 +1,84 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Problem4 {
+
+
+    public Boolean isPalindrome(String input){
+
+        int length = input.length()-1;
+        Boolean palindromCheck= false;
+
+        for (int i = 0; i< length/2; i++){
+
+            if(input.charAt(i)==input.charAt(length-i)){
+                palindromCheck = true;
+            }
+
+        }
+        return palindromCheck;
+    }
+
+    public ArrayList<Character> stringtoCharArray(String input){
+        ArrayList<Character> stringArray = new ArrayList<Character>();
+        for (int i = 0; i < input.length(); i++) {
+            stringArray.add(input.charAt(i));
+        }
+        return stringArray;
+
+    }
+
+    public Boolean isAnagramTest(String input, String input2) {
+
+        ArrayList<Character> stringArray1 = stringtoCharArray(input);
+        ArrayList<Character> stringArray2 = stringtoCharArray(input2);
+        Collections.sort(stringArray1);
+        Collections.sort(stringArray2);
+
+        Boolean anagramCheck = false;
+
+        if (stringArray1.size()!= stringArray2.size()) {
+            anagramCheck = false;
+        } else {
+            int length = input.length();
+            for (int i = 0; i < stringArray1.size(); i++) {
+                if (stringArray1.get(i) == stringArray2.get(i))
+                    anagramCheck = true;
+            }
+        }
+        return anagramCheck;
+    }
+
+    public ArrayList<String> getSubstringFromAString(String input){
+            ArrayList<String> substringList = new ArrayList<String>();
+        for(int i=0; i<input.length()-1; i++){
+            for(int j=0; j<input.length()-i; j++){
+
+                substringList.add(input.substring(j,j+i+1));
+
+            }
+        }
+      return substringList;
+    }
+
+    public  String checkIfAStringHasAPalindonAndANagram(String input){
+        ArrayList<String> substringList = getSubstringFromAString(input);
+
+        String checkResult="NO";
+
+        for(int i=0; i<substringList.size(); i++){
+            String substring1= substringList.get(i);
+
+
+                if(isPalindrome(substring1)){
+                    checkResult="YES";
+                }
+            }
+
+
+        return checkResult;
+    }
+
 }
--- src/test/java/io/zipcoder/Problem4Test.java
+++ src/test/java/io/zipcoder/Problem4Test.java
@@ -1,4 +1,80 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
 public class Problem4Test {
+    Problem4 problem4 = new Problem4();
+    @Test
+    public void isPalindromTest() throws Exception {
+
+        Boolean expected = true;
+        Boolean expected2 = false;
+
+        Boolean actual = problem4.isPalindrome("cabac");
+        Boolean actual2 = problem4.isPalindrome("abac");
+
+
+        Assert.assertEquals(expected2, actual2);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void stringtoCharArrayTest() throws Exception {
+
+        int expected = 5;
+        int expected2 = 2;
+
+        int actual = problem4.stringtoCharArray("cabac").size();
+         int actual2 = problem4.stringtoCharArray("ab").size();
+
+
+       Assert.assertEquals(expected2, actual2);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isAnagramTest() throws Exception {
+
+        Boolean expected = true;
+        Boolean expected2 = false;
+
+        Boolean actual = problem4.isAnagramTest("cabac", "cabac");
+        Boolean actual2 = problem4.isAnagramTest("abd", "ba");
+
+
+        Assert.assertEquals(expected2, actual2);
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void subStringsFromStringTest(){
+
+        int expected =5;
+        int actual = problem4.getSubstringFromAString("abc").size();
+
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+
+    public void checkIfAStringHasAPalindonAndANagramTest(){
+
+        String expected2 = "YES";
+        String expected = "NO";
+        String actual = problem4.checkIfAStringHasAPalindonAndANagram("cdefghmnopqrstuvw");
+        String actual2 = problem4.checkIfAStringHasAPalindonAndANagram("aaabbbb");
+
+
+        Assert.assertEquals(expected2, actual2);
+         Assert.assertEquals(expected, actual);
+    }
+
 }
